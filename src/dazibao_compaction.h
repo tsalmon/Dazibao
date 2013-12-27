1f09522b2df4753fa8df4d7f7c2ae2bbdc756e2d
anthonybt <tonny9998@live.fr>
2013-12-27 21:13:48 +0100

    Update dazibao_compaction.h
    Modification des entêtes

--- src/dazibao_compaction.h
+++ src/dazibao_compaction.h
@@ -1,8 +1,12 @@
 #ifndef DAZIBAO_COMPACTION_H_
 #define DAZIBAO_COMPACTION_H_
 
+#include "dazibao_utilities.h"
+
 unsigned char* convertIntSizeToCharSize(int size);
 
-int deletePad(int f, int taille, int val);
+void callDeletePad(int f, char *file);
+
+int deletePad(int f, int sizeLoop, int val);
 
 #endif
