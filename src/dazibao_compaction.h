b695d94eb75316699369f2096fc118eca75da4b3
anthonybt <tonny9998@live.fr>
2013-12-29 20:48:25 +0100

    Update dazibao_compaction.h
    Suppression d'un header

--- src/dazibao_compaction.h
+++ src/dazibao_compaction.h
@@ -4,8 +4,6 @@
 #include "dazibao_utilities.h"
 #include "dazibao_safe.h"
 
-unsigned char* convertIntSizeToCharSize(int size);
-
 void callDeletePad(int f, char *file);
 
 int deletePad(int f, int sizeLoop, int val);
