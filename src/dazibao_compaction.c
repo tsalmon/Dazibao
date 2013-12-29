98985562bc79df396fbaf758221d7c89437af7e2
anthonybt <tonny9998@live.fr>
2013-12-29 21:04:46 +0100

    Update dazibao_compaction.c
    Mise à jour du fichier:
    -safe_function
    -switch case
    -union des cases 5 et 6

--- src/dazibao_compaction.c
+++ src/dazibao_compaction.c
@@ -7,58 +7,28 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/file.h>
-#include <sys/mman.h>
 #include <errno.h>
 
 #include "dazibao.h"
 #include "dazibao_utilities.h"
-
-unsigned char* convertIntSizeToCharSize(int size) { /* Convertie la taille d'un int sur 3 octets */
-
-	int nombre = size;
-	unsigned char *length = (unsigned char *) malloc (3);
-	char *msg_error_size = "Erreur taille: TLV trop grande\n";
-
-	if( nombre > 16843008 ) {
-		write(STDIN_FILENO, msg_error_size, strlen(msg_error_size));
-	}
-
-	if( nombre > 65792 ) {  
-		length[0] = size / 256; /* Premier octect */
-		size = size % 256;
-	}
-	else {
-		length[0] = 0;
-	}
-
-	if( nombre > 256 ) {
-		length[1] = size / 256; /* Deuxième octet */
-		size = size % 256;
-	} else {
-		length[1] = 0; 
-	}
-	
-	length[2] = size;       /* Troisième octet */
-
-	return length;
-
-}
+#include "dazibao_safe.h"
 
 void callDeletePad(int f, char *file) {
 
 	int shift;
 
-	if(flock(f, LOCK_EX) < 0) { /* Verrou sur le fichier */
+	if (!safe_flock(f, LOCK_EX)) exit(7); /* Verrou sur le fichier */
+
+	if (!safe_lseek(f, 4, SEEK_SET)) exit(4); /* Ignore l'entête */
+	
+	shift = deletePad(f, get_file_size(file), 0); /* Supprime les pad */
+	
+	if (truncate(file, get_file_size(file) - shift) == -1) {  /* Tronque le fichier */
+		perror("Truncate Error:");
 		exit(EXIT_FAILURE);
 	}
 
-	lseek(f, 4, SEEK_SET); /* Ignore l'entête */
-	shift = deletePad(f, get_file_size(file), 0); /* Supprime les pad */
-	truncate(file, get_file_size(file) - shift); /* Tronque le fichier */
-	
-	if(flock(f, LOCK_UN) < 0) { /* Déverrouillage fichier */
-		exit(EXIT_FAILURE);
-	}
+	if (!safe_flock(f, LOCK_UN)) exit(7); /* Déverrouillage fichier */
 
 }
 
@@ -81,92 +51,92 @@ int deletePad(int f, int sizeLoop, int val) {
 	while (((fr = read(f, &buff, 1)) > 0) && (lseek(f,0,SEEK_CUR) <= sizeLoop)) {
 
 		if (buff == 0) { /* TLV PAD1 */
-			
+
 			shift = shift + 1; /* incrémentation de la valeur du décallage de 1 comme pad1 */
 
 		} else {
 
-			read(f, &length, sizeof(length));
-			size = length[0]*256*256 + length[1]*256 + length[2];
+			if (!safe_read(f, &length, sizeof(length))) exit(5);
+			size = length[0] * 256 * 256 + length[1] * 256 + length[2];
 
-			if (buff == 1) { /* TLV PADN */
+			switch(buff) {
+
+				case 1: /* TLV PADN */
 				
-				lseek(f, ( lseek(f, 0, SEEK_CUR) + size) , SEEK_SET);
-				shift = shift + size + 4; /* incrémentation de la valeur du décallage de size + 4 (pour l'entête) comme c'est un padN */
-
-			} else if (buff == 5) { /* TLV COMPOUND */
-
-				tmpPos = lseek(f, 0, SEEK_CUR);
-				shift_start = shift;
-				tmpVal = deletePad(f, (tmpPos + size), shift); /* appel récursif */
-
-				if(tmpVal != 0) {
-
-					lseek(f, tmpPos - (shift + 4), SEEK_SET);
-					shift_end = tmpVal - shift_start; /* calcul du décalage dans un compound */
-					newLength = convertIntSizeToCharSize(size - shift_end); /* nouvelle taille convertie */
-					write(f, &buff, 1);
-					write(f, &newLength[0], 1);
-					write(f, &newLength[1], 1);
-					write(f, &newLength[2], 1);
-					free(newLength);
-
-				}
-
-				lseek(f, (tmpPos + size), SEEK_SET);
-				shift = tmpVal; /* affectation de la nouvelle valeur de décalage */
-
-			} else if (buff == 6) { /* TLV DATED */
-
-				tmpPos = lseek(f, 0, SEEK_CUR);
-				read(f, &date, 4);
-				shift_start = shift;
-				tmpVal = deletePad(f, (tmpPos + size), shift); /* appel récursif */
-
-				if(tmpVal != 0) {
-
-					lseek(f, tmpPos - (shift + 4), SEEK_SET);
-					shift_end = tmpVal - shift_start; /* calcul du décalage dans un dated */
-					newLength = convertIntSizeToCharSize(size - shift_end); /* nouvelle taille convertie */
-					write(f, &buff, 1);
-					write(f, &newLength[0], 1);
-					write(f, &newLength[1], 1);
-					write(f, &newLength[2], 1);
-					write(f, &date, 4);
-					free(newLength);
+					if (!safe_lseek(f, ( lseek(f, 0, SEEK_CUR) + size) , SEEK_SET)) exit(4);
+					shift = shift + size + 4; /* incrémentation de la valeur du décallage de size + 4 (pour l'entête) comme c'est un padN */
 					
-				}
+					break;
 
-				lseek(f, (tmpPos + size), SEEK_SET);
-				shift = tmpVal; /* affectation de la nouvelle valeur de décalage */
+				case 5: /* TLV COMPOUND ET TLV DATED */
+				case 6:
 
-			} else { /* AUTRES TLV */
+					tmpPos = lseek(f, 0, SEEK_CUR);
+					if (tmpPos == - 1) exit(4);
+					
+					if (buff == 6) {
 
-				if( shift == 0 ) { /* Si la valeur du décallage est égale à zéro on saute le bloc */
+						if (!safe_read(f, &date, 4)) exit(5);
 
-					lseek(f, ( lseek(f, 0, SEEK_CUR) + size) , SEEK_SET);
+					}
 
-				} else {   /* Sinon on recopie la TLV à la bonne place */
+					shift_start = shift;
+					tmpVal = deletePad(f, (tmpPos + size), shift); /* appel récursif */
 
-					buf_copy = malloc (size * sizeof(char)); /* allocation mémoire pour stocket le contenu de la TLV */
-					read(f, buf_copy, size);
-					curRead = lseek(f, 0, SEEK_CUR); /* sauvegarde de la position courante de lecture */
-					lseek(f, (lseek(f, 0, SEEK_CUR)) - (shift + size + 4), SEEK_SET); /* repositionnement en avant */ 
-					write(f, &buff, 1);   /* type */
-					write(f, &length, 3); /* taille */
-					write(f, buf_copy, size); /* corps */
-					lseek(f, curRead, SEEK_SET); /* repositionnement au bon emplacement pour lire les TLV suivant */
-					free(buf_copy);
+					if(tmpVal != 0) {
+
+						if (!safe_lseek(f, tmpPos - (shift + 4), SEEK_SET)) exit(4);
+						shift_end = tmpVal - shift_start; /* calcul du décalage dans un compound */
+						newLength = convert_int_to_char(size - shift_end); /* nouvelle taille convertie */
+						if (!safe_write(f, &buff, 1)) exit(6);
+						if (!safe_write(f, &newLength[0], 1)) exit(6);
+						if (!safe_write(f, &newLength[1], 1)) exit(6);
+						if (!safe_write(f, &newLength[2], 1)) exit(6);
+
+						if (buff == 6) {
+							
+							if (!safe_write(f, &date, 4)) exit(6);
+
+						}
+
+						safe_free(newLength);
+
+					}
+
+					if (!safe_lseek(f, (tmpPos + size), SEEK_SET)) exit(4);
+					shift = tmpVal; /* affectation de la nouvelle valeur de décalage */
+					break;
+
+				default: /* AUTRES TLV */
+
+					if( shift == 0 ) { /* Si la valeur du décallage est égale à zéro on saute le bloc */
+
+						if (!safe_lseek(f, ( lseek(f, 0, SEEK_CUR) + size) , SEEK_SET)) exit(4);
+
+					} else {   /* Sinon on recopie la TLV à la bonne place */
+
+						buf_copy = safe_malloc (size * sizeof(char)); /* allocation mémoire pour stocket le contenu de la TLV */
+						if (buf_copy == NULL) exit(1);
+						if (!safe_read(f, buf_copy, size)) exit(5);
+						curRead = lseek(f, 0, SEEK_CUR); /* sauvegarde de la position courante de lecture */
+						if (curRead < 0) exit(4);
+						if (!lseek(f, (curRead) - (shift + size + 4), SEEK_SET)) exit(4); /* repositionnement en avant */ 
+						if (!safe_write(f, &buff, 1)) exit(6);          /* type */
+						if (!safe_write(f, &length, 3)) exit(6);        /* taille */
+						if (!safe_write(f, buf_copy, size)) exit(6);    /* corps */
+						if (!lseek(f, curRead, SEEK_SET)) exit(4); /* repositionnement au bon emplacement pour lire les TLV suivant */
+						safe_free(buf_copy);
+
+					}
+
+					break;
 
 				}
 
 			}
 
 		}
-			 
-	}
 
 	return(shift);
 
 }
-
